aeefb6aa9457ab6635eda7c23ead2dcac90bdea2
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-09-07 22:39:19 +0300

    Project Init

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { DefaultSession, NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github";
+
+declare module "next-auth" {
+  interface Session {
+    user: DefaultSession["user"] & {
+      id: string;
+    };
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db),
+  callbacks: {
+    session: ({ session, user }) => {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  providers: [
+     GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    })
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
