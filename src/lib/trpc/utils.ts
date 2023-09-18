fed2b92a2a697beb5120fe466f98108f9a68fa22
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-09-18 10:27:27 +0300

    Fix Vercel_URL

--- src/lib/trpc/utils.ts
+++ src/lib/trpc/utils.ts
@@ -1,10 +1,9 @@
-import { env } from "@/lib/env.mjs";
 function getBaseUrl() {
-  if (typeof window !== "undefined") return "";
-  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
-  return "http://localhost:3000";
+  if (typeof window !== 'undefined') return ''
+  if (process.env.VERCEL) return `https://${process.env.VERCEL_URL}`
+  return 'http://localhost:3000'
 }
 
 export function getUrl() {
-  return getBaseUrl() + "/api/trpc";
+  return getBaseUrl() + '/api/trpc'
 }
