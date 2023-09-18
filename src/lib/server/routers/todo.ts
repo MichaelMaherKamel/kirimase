37702880b3a559523334b67a18902847f9813684
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-09-18 08:16:42 +0300

    Fixing Todo mismatch names

--- src/lib/server/routers/todo.ts
+++ src/lib/server/routers/todo.ts
@@ -1,32 +1,22 @@
-import { getTodById, getTodo } from "@/lib/api/todo/queries";
-import { publicProcedure, router } from "../trpc";
-import {
-  todIdSchema,
-  insertTodParams,
-  updateTodParams,
-} from "@/lib/db/schema/todo";
-import { createTod, deleteTod, updateTod } from "@/lib/api/todo/mutations";
+import { getTodById, getTods } from '@/lib/api/todo/queries'
+import { publicProcedure, router } from '../trpc'
+import { todIdSchema, insertTodParams, updateTodParams } from '@/lib/db/schema/todo'
+import { createTod, deleteTod, updateTod } from '@/lib/api/todo/mutations'
 
 export const todoRouter = router({
   getTodo: publicProcedure.query(async () => {
-    return getTodo();
+    return getTods()
   }),
   getTodById: publicProcedure.input(todIdSchema).query(async ({ input }) => {
-    return getTodById(input.id);
+    return getTodById(input.id)
   }),
-  createTod: publicProcedure
-    .input(insertTodParams)
-    .mutation(async ({ input }) => {
-      return createTod(input);
-    }),
-  updateTod: publicProcedure
-    .input(updateTodParams)
-    .mutation(async ({ input }) => {
-      return updateTod(input.id, input);
-    }),
-  deleteTod: publicProcedure
-    .input(todIdSchema)
-    .mutation(async ({ input }) => {
-      return deleteTod(input.id);
-    }),
-});
+  createTod: publicProcedure.input(insertTodParams).mutation(async ({ input }) => {
+    return createTod(input)
+  }),
+  updateTod: publicProcedure.input(updateTodParams).mutation(async ({ input }) => {
+    return updateTod(input.id, input)
+  }),
+  deleteTod: publicProcedure.input(todIdSchema).mutation(async ({ input }) => {
+    return deleteTod(input.id)
+  }),
+})
