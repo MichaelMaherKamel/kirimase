91c4d76e8191b6544990f43478ae9e2db78fe537
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-09-18 12:54:02 +0300

    Removed ToDOs

--- src/lib/server/routers/_app.ts
+++ src/lib/server/routers/_app.ts
@@ -2,13 +2,11 @@ import { computersRouter } from './computers'
 import { router } from '../trpc'
 import { siteRouter } from './site'
 import { userRouter } from './users'
-import { todoRouter } from './todo'
 
 export const appRouter = router({
   computers: computersRouter,
   site: siteRouter,
   users: userRouter,
-  todo: todoRouter,
 })
 
 export type AppRouter = typeof appRouter
